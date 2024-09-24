f39630bbeebe1330e794eb59f106c0e0c1d97896
SergioSuaSan <148342257+SergioSuaSan@users.noreply.github.com>
2024-09-24 19:51:46 +0200

    JuegodeNumerosArreglado

--- JuegoDeNumerosJPC/app/src/main/java/net/azarquiel/juegodenumerosjpc/MainActivity.kt
+++ JuegoDeNumerosJPC/app/src/main/java/net/azarquiel/juegodenumerosjpc/MainActivity.kt
@@ -1,7 +1,6 @@
 package net.azarquiel.juegodenumerosjpc
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -23,7 +22,8 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -31,48 +31,42 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import net.azarquiel.juegodenumerosjpc.ui.theme.JuegoDeNumerosJPCTheme
-import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
 
-    private var azar: Int = 0
-    private var contador: Int = 0
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        var random = Random(seed = System.currentTimeMillis())
-        azar = (1..100).random(random)
-        Log.d("sergio","$azar")
 
         setContent {
             JuegoDeNumerosJPCTheme {
-                MainScreen()
+                MainScreen(MyViewModel())
             }
         }
     }
 
-    @Preview(showBackground = true)
+    //@Preview(showBackground = true)
     @Composable
-    private fun MainScreen() {
+    private fun MainScreen(myViewModel: MyViewModel) {
         Scaffold(
             // Barra superior
             topBar = { CustomTopBar() },
             // Contenido principal
             content = { padding ->
-                CustomContent(padding, )
+                CustomContent(padding, myViewModel)
             }
         )
     }
 
     @Composable
-    fun CustomContent(padding: PaddingValues) {
-
+    fun CustomContent(padding: PaddingValues, myViewModel: MyViewModel) {
+        val intentillo by myViewModel.intentos.observeAsState(0)
         Row (
             modifier = Modifier
                 .fillMaxSize()
@@ -82,7 +76,7 @@ class MainActivity : ComponentActivity() {
         ){
 
             Text(
-                text = "Intentos: $contador",
+                text = "Intentos: $intentillo",
                 fontSize = 20.sp,
                 fontWeight = FontWeight.Bold,
                 modifier = Modifier.padding(10.dp)
@@ -101,15 +95,15 @@ class MainActivity : ComponentActivity() {
             // Contenido de la aplicación
             content = {
 
-                Contenido(this@MainActivity.azar)
+                Contenido(myViewModel)
 
             }
         )
     }
 
     @Composable
-    fun Contenido( azar: Int) {
-        var n by rememberSaveable { mutableStateOf(50)}
+    fun Contenido(myViewModel: MyViewModel) {
+        var n by rememberSaveable { mutableIntStateOf(50)}
 
 
 
@@ -131,14 +125,18 @@ class MainActivity : ComponentActivity() {
         ) {
             Row(verticalAlignment = Alignment.CenterVertically) {
                 Button(
-                    onClick = { n--; this@MainActivity.contador++; comparacion(n, azar) },
+                    onClick = { n--
+                                myViewModel.incrementarIntentos()
+                                comparacion(n, myViewModel) },
                     colors = ButtonDefaults.buttonColors(colorResource(R.color.purple_700))
                 ) {
                     Text("-1")
                 }
                 Spacer(modifier = Modifier.padding(8.dp))
                 Button(
-                    onClick = { n++; this@MainActivity.contador++; comparacion(n, azar)  },
+                    onClick = { n++
+                        myViewModel.incrementarIntentos()
+                        comparacion(n, myViewModel)  },
                     colors = ButtonDefaults.buttonColors(colorResource(R.color.purple_700))
                 ) {
                     Text("+1")
@@ -147,14 +145,18 @@ class MainActivity : ComponentActivity() {
             }
             Row(verticalAlignment = Alignment.CenterVertically) {
                 Button(
-                    onClick = { n-=5; this@MainActivity.contador++; comparacion(n, azar)  },
+                    onClick = { n-=5
+                        myViewModel.incrementarIntentos()
+                        comparacion(n, myViewModel)  },
                     colors = ButtonDefaults.buttonColors(colorResource(R.color.purple_700))
                 ) {
-                    Text("-5 ${this@MainActivity.contador}")
+                    Text("-5 ")
                 }
                 Spacer(modifier = Modifier.padding(8.dp))
                 Button(
-                    onClick = { n+=5; this@MainActivity.contador++ ; comparacion(n, azar) },
+                    onClick = { n+=5
+                        myViewModel.incrementarIntentos()
+                        comparacion(n, myViewModel) },
                     colors = ButtonDefaults.buttonColors(colorResource(R.color.purple_700))
                 ) {
                     Text("+5")
@@ -163,14 +165,18 @@ class MainActivity : ComponentActivity() {
             }
             Row(verticalAlignment = Alignment.CenterVertically) {
                 Button(
-                    onClick = { n-=10; this@MainActivity.contador++; comparacion(n, azar)  },
+                    onClick = { n-=10
+                        myViewModel.incrementarIntentos()
+                        comparacion(n, myViewModel)  },
                     colors = ButtonDefaults.buttonColors(colorResource(R.color.purple_700))
                 ) {
                     Text("-10")
                 }
                 Spacer(modifier = Modifier.padding(8.dp))
                 Button(
-                    onClick = { n+=10; this@MainActivity.contador++; comparacion(n, azar)  },
+                    onClick = { n+=10
+                        myViewModel.incrementarIntentos()
+                        comparacion(n, myViewModel)  },
                     colors = ButtonDefaults.buttonColors(colorResource(R.color.purple_700))
                 ) {
                     Text("+10")
@@ -180,10 +186,10 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    private fun comparacion(n: Int, azar: Int) {
-        if (n < this.azar) {
+    private fun comparacion(n: Int, myViewModel: MyViewModel) {
+        if (n < myViewModel.numeroAleatorio.value!!) {
             Toast.makeText(this, "El número es mayor", Toast.LENGTH_SHORT).show()
-        } else if (n > this.azar) {
+        } else if (n > myViewModel.numeroAleatorio.value!!) {
             Toast.makeText(this, "El número es menor", Toast.LENGTH_SHORT).show()
         } else {
             Toast.makeText(this, "Has acertado", Toast.LENGTH_SHORT).show()        }
--- JuegoDeNumerosJPC/app/src/main/java/net/azarquiel/juegodenumerosjpc/MyViewModel.kt
+++ JuegoDeNumerosJPC/app/src/main/java/net/azarquiel/juegodenumerosjpc/MyViewModel.kt
@@ -7,4 +7,18 @@ import androidx.lifecycle.ViewModel
 class MyViewModel: ViewModel() {
     private val _numeroAleatorio = MutableLiveData<Int>()
     val numeroAleatorio: LiveData<Int> = _numeroAleatorio
+
+    init {
+        _numeroAleatorio.value = (1..100).random()
+    }
+
+    private val _intentos = MutableLiveData<Int>()
+    val intentos: LiveData<Int> = _intentos
+
+    fun incrementarIntentos() {
+        _intentos.value = (_intentos.value ?: 0) + 1
+    }
+
+
+
 }
